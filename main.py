814261fdecc9ddd9c1cd17a0d9a428f70fc6a4da
Erionis <erion.islamay@gmail.com>
2024-01-22 16:54:31 +0100

    data validation added

--- main.py
+++ main.py
@@ -14,3 +14,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+     
+     
+   
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
